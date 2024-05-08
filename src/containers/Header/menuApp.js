46a178220013c716bfbc77dacf477957116abef5
choemxin1 <160872427+choemxin1@users.noreply.github.com>
2024-05-08 18:07:17 +0700

    Update menuApp.js

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -24,9 +24,7 @@ export const adminMenu = [
             { //quản lý kế hoạch khám bệnh của bác sỹ
                 name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
             },
-            { //quản lý bệnh nhân khám bệnh của bác sỹ
-                name: 'menu.doctor.manage-patient', link: '/doctor/manage-patient'
-            },
+            
 
         ]
     },
@@ -60,9 +58,7 @@ export const doctorMenu = [
     {
         name: 'menu.admin.manage-user',
         menus: [
-            { //quản lý kế hoạch khám bệnh của bác sỹ
-                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
-            },
+            
             { //quản lý bệnh nhân khám bệnh của bác sỹ
                 name: 'menu.doctor.manage-patient', link: '/doctor/manage-patient'
             },
